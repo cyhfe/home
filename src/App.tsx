313f4d3eb14fd5c0e03c8d0fef07d8bc6e927f72
chenyuhao <cccchenyuhao@gmail.com>
2023-11-23 19:00:10 +0800

    home

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,9 @@
-import "./App.css";
-import { css } from "@emotion/react";
-
-const linkContainer = css`
-  display: flex;
-  column-gap: 10px;
-`;
-
-const titleStyle = css`
-  width: 300px;
-  text-align: right;
-`;
-
-const linkStyle = css`
-  width: 300px;
-  text-align: left;
-`;
-
-const data = [
-  {
-    link: "http://ui.icyh.me",
-    label: "COMPONENT LIBRARY",
-  },
-  {
-    link: "http://ui.icyh.me",
-    label: " D3 REACT VISUALIZATION",
-  },
-  {
-    link: "http://ui.icyh.me",
-    label: "BLOG",
-  },
-  {
-    link: "http://ui.icyh.me",
-    label: "GITHUB",
-  },
-];
+import Home from "./Home.mdx";
 
 function App() {
   return (
-    <div>
-      {data.map((d) => {
-        return (
-          <div css={linkContainer}>
-            <div css={titleStyle}>{d.label}</div>
-            <div css={linkStyle}>
-              <a target="_blank" href={d.link}>
-                {d.link}
-              </a>
-            </div>
-          </div>
-        );
-      })}
+    <div className="prose prose-slate p-4">
+      <Home />
     </div>
   );
 }
